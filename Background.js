996ba060bfb22d1da9492eb03eac90a2e16b8176
RobinKnol <robin@knol.email>
2020-01-24 11:28:35 +0100

    Initial commit

---
+++ Background.js
@@ -0,0 +1,11 @@
+// maak een variabele voor de canvas
+const canvas = document.getElementById('canvas');
+
+// leg een 2d-context over de canvas
+const context = canvas.getContext('2d');
+
+//maak de canvas schermvullend
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
