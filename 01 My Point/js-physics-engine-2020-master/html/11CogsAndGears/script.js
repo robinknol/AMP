1830919c8df83895cd2ab83308e08f371019bbfe
Robin Knol <robin@knol.email>
2020-09-07 15:07:30 +0200

    11cogsAndGears

--- 01 My Point/js-physics-engine-2020-master/html/11CogsAndGears/script.js
+++ 01 My Point/js-physics-engine-2020-master/html/11CogsAndGears/script.js
@@ -8,17 +8,35 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
+let img,img2,angle;
 
-// create namespace
-let img = new Image();
+img = new Image();
 img.src = "images/cog.png";
 
-img.addEventListener('load',function(){
-    animate()
+angle = 0.1;
+
+img.addEventListener('load',()=>
+{
+    setInterval(animate,10);
 })
 
-function animate(){
-  requestAnimationFrame(animate)
-  context.clearRect(0,0,width,height);
-  context.drawImage(img,0,0,200,200)
+function animate()
+{
+    context.clearRect(0,0,width,height);
+    
+    context.save();
+    context.translate(300,300);
+    context.rotate(0);
+    context.rotate(angle);
+    context.drawImage(img,-300,-300,600,600);
+    context.restore();
+
+    context.save();
+    context.translate(850,300);
+    context.rotate(50);
+    context.rotate(-angle);
+    context.drawImage(img,-300,-300,600,600);
+    context.restore();
+
+    angle += 0.01;
 }
