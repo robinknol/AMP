b6c53b803d239f40feaf151640b776eb30db6b95
Robin Knol <robin@knol.email>
2020-10-19 14:39:12 +0200

    17 SpriteSheet

--- 01 My Point/js-lib/html/13MyClock/script.js
+++ 01 My Point/js-lib/html/13MyClock/script.js
@@ -44,7 +44,7 @@ function animate()
 
     context.save();
     context.translate(Clock.width / 2,Clock.height / 2);
-    context.rotate(hours * 2 * Math.PI / 12);
+    context.rotate(hours * 2 * Math.PI / 12 + minutes / 720);
     context.drawImage(HourHand, -HourHand.width / 2, -HourHand.height)
     context.restore();
 
